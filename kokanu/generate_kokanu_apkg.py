d77e113d640dee30a3d25378994e601ff9b57986
chrisperalta <chris99994@gmail.com>
2025-08-15 23:40:04 -0600

    Added tags, better formatting, and random sorting

--- kokanu/generate_kokanu_apkg.py
+++ kokanu/generate_kokanu_apkg.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import genanki
 import html
-
 from enum import Enum
 
 
@@ -25,6 +24,10 @@ my_css = """
   color: white;
   background-color: #000000;
 }
+.pos {
+    font-size: 12px;
+    text-transform: lowercaseaan;
+}
 """
 
 model = genanki.Model(
@@ -34,22 +37,22 @@ model = genanki.Model(
     templates=[
         {
             'name': 'Card 1',
-            'qfmt': '{{' + Field.WORD.value + '}}<br>{{' + Field.TYPE.value + '}}<br>',
+            'qfmt': '{{' + Field.WORD.value + '}}<br><div class="pos">{{' + Field.TYPE.value + '}}</div><br>',
             'afmt': '{{FrontSide}}<hr id="answer"> {{' + Field.MEANING.value + ' }}',
         },
         {
             'name': 'Card 2',
-            'qfmt': '{{' + Field.WORD.value + '}}<br>' + Field.NOUN.name + '<br>',
+            'qfmt': '{{' + Field.WORD.value + '}}<br><div class="pos">' + Field.NOUN.name + '</div><br>',
             'afmt': '{{FrontSide}}<hr id="answer"> {{' + Field.NOUN.value + ' }}',
         },
         {
             'name': 'Card 3',
-            'qfmt': '{{' + Field.WORD.value + '}}<br>' + Field.VERB.name + '<br>',
+            'qfmt': '{{' + Field.WORD.value + '}}<br><div class="pos">' + Field.VERB.name + '</div><br>',
             'afmt': '{{FrontSide}}<hr id="answer"> {{' + Field.VERB.value + ' }}',
         },
         {
             'name': 'Card 4',
-            'qfmt': '{{' + Field.MODIFIER.value + '}}<br>' + Field.MODIFIER.name + '<br>',
+            'qfmt': '{{' + Field.MODIFIER.value + '}}<br><div class="pos">' + Field.MODIFIER.name + '</div><br>',
             'afmt': '{{FrontSide}}<hr id="answer"> {{' + Field.MODIFIER.value + ' }}',
         },
         {
@@ -63,7 +66,8 @@ model = genanki.Model(
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTVGXFd17kcvfu__zjshqiV3kW360IclOEfEdWda_K6ZCg4TY6nW2Gwn4_bs1yQeFLwrZI1_xEvSuP0/pub?gid=0&single=true&output=csv'
 
-df = pd.read_csv(url).fillna('')
+# Read google sheet and sort at random due to no word frequency data
+df = pd.read_csv(url).fillna('').sample(frac=1).reset_index(drop=True)
 
 kokanu_deck = genanki.Deck(
     4564564565445,
@@ -72,7 +76,9 @@ kokanu_deck = genanki.Deck(
 for index, row in df.iterrows():
     note = genanki.Note(
         model=model,
-        fields=list(map(lambda x: html.escape(row[x.value]), Field)))
+        fields=list(map(lambda x: html.escape(row[x.value]), Field)),
+        tags=[row[Field.TYPE.value].replace(' ', ''), row[Field.FAMILY.value].replace(' ', '')]
+    )
     kokanu_deck.add_note(note)
 
 genanki.Package(kokanu_deck).write_to_file('kokanu/kokanu_deck.apkg')
